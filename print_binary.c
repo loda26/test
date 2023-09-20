c06fd1835fa6056832ebc5f2da74dec4abae0953
loda26 <mokhaled26498@gmail.com>
2023-09-20 07:29:32 -0700

    q

---
+++ print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_binary - print binary number
+*@list: list of arguments
+*Return: counter
+*/
+int print_binary(va_list list)
+{
+	int num = va_arg(list, unsigned int);
+	int array[32];
+	int n;
+	int counter = 0;
+	if (num  == 0)
+	{
+		_putchar('0');
+		return (counter);
+	}
+	while (num > 0)
+	{
+		array[counter++] = num % 2;
+		num /= 2;
+	}
+	for (n = counter - 1; n >= 0; n--)
+		_putchar(array[n] + '0');
+
+	return (counter);
+}
